d242f75fa3405ac0ceea9aeff83d205703094b13
NedaKovacevic <neda.n.kovacevic@gmail.com>
2022-05-25 21:16:24 +0200

    4

--- Biletarnica/Glavna.Designer.cs
+++ Biletarnica/Glavna.Designer.cs
@@ -29,29 +29,14 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.dgvTabela = new System.Windows.Forms.DataGridView();
             this.biletarnica0DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.biletarnica0DataSet = new Biletarnica.Biletarnica0DataSet();
             this.buttonKupi = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvTabela)).BeginInit();
+            this.dgvTabela = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.biletarnica0DataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.biletarnica0DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabela)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dgvTabela
-            // 
-            this.dgvTabela.AllowUserToAddRows = false;
-            this.dgvTabela.AllowUserToDeleteRows = false;
-            this.dgvTabela.AutoGenerateColumns = false;
-            this.dgvTabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTabela.DataSource = this.biletarnica0DataSetBindingSource;
-            this.dgvTabela.Location = new System.Drawing.Point(122, 169);
-            this.dgvTabela.Name = "dgvTabela";
-            this.dgvTabela.ReadOnly = true;
-            this.dgvTabela.RowHeadersWidth = 102;
-            this.dgvTabela.RowTemplate.Height = 40;
-            this.dgvTabela.Size = new System.Drawing.Size(962, 728);
-            this.dgvTabela.TabIndex = 0;
             // 
             // biletarnica0DataSetBindingSource
             // 
@@ -72,28 +57,40 @@
             this.buttonKupi.Text = "kupi";
             this.buttonKupi.UseVisualStyleBackColor = true;
             // 
+            // dgvTabela
+            // 
+            this.dgvTabela.AllowUserToAddRows = false;
+            this.dgvTabela.AllowUserToDeleteRows = false;
+            this.dgvTabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTabela.Location = new System.Drawing.Point(176, 176);
+            this.dgvTabela.Name = "dgvTabela";
+            this.dgvTabela.ReadOnly = true;
+            this.dgvTabela.RowHeadersWidth = 102;
+            this.dgvTabela.RowTemplate.Height = 40;
+            this.dgvTabela.Size = new System.Drawing.Size(759, 610);
+            this.dgvTabela.TabIndex = 2;
+            // 
             // Glavna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1508, 937);
-            this.Controls.Add(this.buttonKupi);
             this.Controls.Add(this.dgvTabela);
+            this.Controls.Add(this.buttonKupi);
             this.Name = "Glavna";
             this.Text = "Admin";
             this.Load += new System.EventHandler(this.Glavna_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvTabela)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.biletarnica0DataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.biletarnica0DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTabela)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dgvTabela;
         private System.Windows.Forms.BindingSource biletarnica0DataSetBindingSource;
         private Biletarnica0DataSet biletarnica0DataSet;
         private System.Windows.Forms.Button buttonKupi;
+        private System.Windows.Forms.DataGridView dgvTabela;
     }
 }

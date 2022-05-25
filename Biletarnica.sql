dc7ec58a36599ed82947764c2063395580fa08d6
NedaKovacevic <neda.n.kovacevic@gmail.com>
2022-05-25 19:29:41 +0200

    1

---
+++ Biletarnica.sql
@@ -0,0 +1,58 @@
+create database Biletarnica0
+use Biletarnica0
+
+create table korisnik(
+id int primary key identity(1,1),
+ime nvarchar(50) not null,
+prezime nvarchar(50) not null,
+email nvarchar(50) not null,
+pass nvarchar(100) not null
+)
+drop table koncert
+create table koncert(
+	id int primary key identity(1,1),
+	izvodjac nvarchar(50),
+	lokacija nvarchar(30),
+	dan date,
+	vreme time
+)
+
+create table mesta
+(
+	id int primary key identity(1,1),
+	naziv nvarchar(50),
+	cena int not null,
+	slobodnih int
+)
+drop table rezervacija
+create table rezervacija
+(
+	id int identity(1,1) primary key,
+	korisnikID int foreign key references korisnik(id),
+	koncertID int foreign key references koncert(id),
+	mestaID int foreign key references mesta(id),
+)
+
+
+
+insert into mesta values
+('parter', 4000, 10000),
+('fanpit', 7000, 5000),
+('sedenje', 3500, 15000),
+('loza', 12000, 100)
+
+insert into koncert values
+('Metallica', 'Marakana', '2022-06-20', '18:30'),
+('Black Sabbath', 'Arena', '2022-09-12', '20:00'),
+('Goblini', 'Sajam', '2022-09-27', '17:00'),
+('Iron Maiden', 'Arena', '2022-05-24','19:30')
+
+insert into Korisnik values
+('Stanoje', 'Petrovic', 'stan.petrovic@gmail.com', '1234'), 
+('Milica', 'Lazetic', 'abvgd@yahoo.com', '5555'),
+('Jovana', 'Milic', 'lalalala@gmail.com', 'zebra'),
+('Neda', 'Kovacevic', 'nedak@gmail.com', '0000')
+
+
+
+drop database Biletarnica0
---
+++ Biletarnica/Login.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Biletarnica
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBoxMail.Text == "" || textBoxPass.Text == "")
+            {
+                MessageBox.Show("Unesite E-mail i Password!");
+                return;
+            }
+            else // lako se razdvoje slucajevi admin i korisnik, ako je email simikic.nikola@gmail.com
+                 // onda idi na admin page, a ako nije, idi na user page
+            {
+                try
+                {
+                    SqlConnection veza = Konekcija.Connect();
+                    SqlCommand komanda = new SqlCommand("SELECT * FROM Korisnik WHERE email = @username", veza);
+                    komanda.Parameters.AddWithValue("@username", textBoxMail.Text);
+                    SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+
+                    DataTable tabela = new DataTable();
+                    adapter.Fill(tabela);
+
+                    int brojac = tabela.Rows.Count;
+                    if (brojac == 1)
+                    {
+                        if (String.Compare(tabela.Rows[0]["pass"].ToString(), textBoxPass.Text) == 0)
+                        {
+                            MessageBox.Show("Login Successful!");
+                            Program.user_ime = tabela.Rows[0]["ime"].ToString();
+                            Program.user_prezime = tabela.Rows[0]["prezime"].ToString();
+
+                            this.Hide();
+                            Admin frm_Admin = new Admin();
+                            frm_Admin.Show();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Neispravna Lozinka!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nepostojeci E-mail!");
+                    }
+                }
+                catch (Exception Greska)
+                {
+                    MessageBox.Show(Greska.Message);
+                }
+            }
+        }
+    }
+}
